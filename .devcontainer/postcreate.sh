46633843b2e571f9c0e6dc4a1db3b2efb39e8b42
Kaleb-Bruwer <kaleb.bruwer@gmail.com>
2022-07-07 15:33:01 +0200

    Enzyme-friendly container

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,3 +1,8 @@
+sudo chown vscode ./enzyme/build
+sudo ln -s /usr/bin/clang-12 /usr/bin/clang
+sudo ln -s /usr/lib/llvm-12/bin/opt /usr/bin/opt
+
+sudo apt update
 sudo apt install libsdl2-dev -y
 sudo apt install libsdl2-image-dev -y
 sudo apt install libsdl2-ttf-dev -y
