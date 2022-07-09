74f1e88f0c8b1614e1f991c1882f1eb631f29bf3
Kaleb-Bruwer <kaleb.bruwer@gmail.com>
2022-07-09 14:34:58 +0200

    Enzyme-friendly build process

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,9 +1,5 @@
-sudo chown vscode ./enzyme/build
-sudo ln -s /usr/bin/clang-12 /usr/bin/clang
-sudo ln -s /usr/lib/llvm-12/bin/opt /usr/bin/opt
-
-sudo apt update
-sudo apt install libsdl2-dev -y
-sudo apt install libsdl2-image-dev -y
-sudo apt install libsdl2-ttf-dev -y
-sudo apt install libsdl2-mixer-dev -y
+apt-get update
+apt-get install libsdl2-dev -y
+apt-get install libsdl2-image-dev -y
+apt-get install libsdl2-ttf-dev -y
+apt-get install libsdl2-mixer-dev -y
